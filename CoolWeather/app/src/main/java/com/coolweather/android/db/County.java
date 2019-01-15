437909b8a5dbd806429a725884328aa35e90bcb0
曹孟 <clarence.cao@bizfocus.cn>
2019-01-15 10:41:43 +0800

    @CM 省市级联选择.

--- CoolWeather/app/src/main/java/com/coolweather/android/db/County.java
+++ CoolWeather/app/src/main/java/com/coolweather/android/db/County.java
@@ -8,7 +8,7 @@ public class County extends DataSupport {
 
     private String countyName;
 
-    private int weatherId;
+    private String weatherId;
 
     private int cityId;
 
@@ -28,11 +28,11 @@ public class County extends DataSupport {
         this.countyName = countyName;
     }
 
-    public int getWeatherId() {
+    public String getWeatherId() {
         return weatherId;
     }
 
-    public void setWeatherId(int weatherId) {
+    public void setWeatherId(String weatherId) {
         this.weatherId = weatherId;
     }
 
